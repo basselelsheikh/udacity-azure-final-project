1520c0f1cb590bb5545e38a784e849a9a43a504b
Bassel El-Sheikh <bassel.elsheikh@gmail.com>
2025-12-06 22:46:30 +0300

    updates

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -4,41 +4,17 @@ import redis
 import socket
 import logging
 
-# ===========================
-# TODO: Import required libraries for App Insights
-# ===========================
 from opencensus.ext.azure.log_exporter import AzureLogHandler
-from opencensus.ext.azure.metrics_exporter import new_metrics_exporter
+from opencensus.ext.azure import metrics_exporter
 from opencensus.ext.azure.trace_exporter import AzureExporter
 from opencensus.ext.flask.flask_middleware import FlaskMiddleware
-from opencensus.trace.samplers import AlwaysOnSampler
+from opencensus.trace.samplers import ProbabilitySampler
 from opencensus.trace.tracer import Tracer
-from opencensus.ext.azure.common import telemetry
 
-APP_INSIGHTS_CONN_STRING = "InstrumentationKey=c949c2c4-6566-4517-999d-5043c81d94ed;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/;LiveEndpoint=https://westus.livediagnostics.monitor.azure.com/;ApplicationId=e8772aa2-0f98-4c0f-ba55-f0f89fb13833"
+APP_INSIGHTS_KEY = "c949c2c4-6566-4517-999d-5043c81d94ed"
 
 app = Flask(__name__)
 app.config.from_pyfile('config_file.cfg')
-
-logger = logging.getLogger(__name__)
-logger.addHandler(AzureLogHandler(connection_string=APP_INSIGHTS_CONN_STRING))
-logger.setLevel(logging.INFO)
-
-metrics = new_metrics_exporter(
-    enable_standard_metrics=True,
-    connection_string=APP_INSIGHTS_CONN_STRING
-)
-
-tracer = Tracer(
-    exporter=AzureExporter(connection_string=APP_INSIGHTS_CONN_STRING),
-    sampler=AlwaysOnSampler()
-)
-
-middleware = FlaskMiddleware(
-    app,
-    exporter=AzureExporter(connection_string=APP_INSIGHTS_CONN_STRING),
-    sampler=AlwaysOnSampler()
-)
 
 button1 = os.environ.get('VOTE1VALUE', app.config['VOTE1VALUE'])
 button2 = os.environ.get('VOTE2VALUE', app.config['VOTE2VALUE'])
@@ -48,18 +24,39 @@ if app.config.get('SHOWHOST', 'false') == "true":
     title = socket.gethostname()
 
 r = redis.Redis()
-if not r.get(button1):
-    r.set(button1, 0)
-if not r.get(button2):
-    r.set(button2, 0)
+if not r.get(button1): r.set(button1, 0)
+if not r.get(button2): r.set(button2, 0)
 
-event_exporter = AzureExporter(connection_string=APP_INSIGHTS_CONN_STRING)
+logger = logging.getLogger(__name__)
+logger.addHandler(AzureLogHandler(connection_string=f'InstrumentationKey={APP_INSIGHTS_KEY}'))
+logger.setLevel(logging.INFO)
+
+exporter = metrics_exporter.new_metrics_exporter(
+    enable_standard_metrics=True,
+    connection_string=f'InstrumentationKey={APP_INSIGHTS_KEY}'
+)
+
+tracer = Tracer(
+    exporter=AzureExporter(connection_string=f'InstrumentationKey={APP_INSIGHTS_KEY}'),
+    sampler=ProbabilitySampler(1.0)
+)
+
+middleware = FlaskMiddleware(
+    app,
+    exporter=AzureExporter(connection_string=f'InstrumentationKey={APP_INSIGHTS_KEY}'),
+    sampler=ProbabilitySampler(1.0)
+)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         vote1 = r.get(button1).decode('utf-8')
+        with tracer.span(name="Cats Vote GET"):
+            pass
         vote2 = r.get(button2).decode('utf-8')
+        with tracer.span(name="Dogs Vote GET"):
+            pass
+
         return render_template(
             "index.html",
             value1=int(vote1),
@@ -75,12 +72,11 @@ def index():
         if vote == 'reset':
             r.set(button1, 0)
             r.set(button2, 0)
-
             vote1 = r.get(button1).decode('utf-8')
             vote2 = r.get(button2).decode('utf-8')
 
-            logger.info('Cats vote reset.', extra={"custom_dimensions": {"Cats Vote": vote1}})
-            logger.info('Dogs vote reset.', extra={"custom_dimensions": {"Dogs Vote": vote2}})
+            logger.info('Cats vote reset', extra={"custom_dimensions": {"Cats Vote": vote1}})
+            logger.info('Dogs vote reset', extra={"custom_dimensions": {"Dogs Vote": vote2}})
 
             return render_template(
                 "index.html",
@@ -90,20 +86,13 @@ def index():
                 button2=button2,
                 title=title
             )
-
         else:
             r.incr(vote, 1)
 
             with tracer.span(name=f"Vote Recorded: {vote}"):
                 pass
 
-            logger.info(f"{vote} vote recorded.", extra={"custom_dimensions": {"Voted For": vote}})
-
-            event = telemetry.Event(
-                name=f"{vote}Clicked",
-                properties={"vote": vote}
-            )
-            event_exporter.export([event])
+            logger.info(f'{vote} vote clicked', extra={"custom_dimensions": {"Voted For": vote}})
 
             vote1 = r.get(button1).decode('utf-8')
             vote2 = r.get(button2).decode('utf-8')
