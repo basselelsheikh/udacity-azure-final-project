486287258b2c3f67f6e4e4797924e9ef1ad8b1e7
Bassel El-Sheikh <belsheikh@ejada.com>
2025-12-07 13:11:33 +0300

    updates

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -11,7 +11,20 @@ from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 from opencensus.trace.samplers import ProbabilitySampler
 from opencensus.trace.tracer import Tracer
 
+from opencensus.ext.azure.log_exporter import AzureEventHandler
+from opencensus.stats import aggregation as aggregation_module
+from opencensus.stats import measure as measure_module
+from opencensus.stats import stats as stats_module
+from opencensus.stats import view as view_module
+from opencensus.tags import tag_map as tag_map_module
+from opencensus.trace import config_integration
+
 APP_INSIGHTS_KEY = "86dff2e7-9c85-4a73-acbc-e35a19065cee"
+
+stats = stats_module.stats
+view_manager = stats.view_manager
+config_integration.trace_integrations(['logging'])
+config_integration.trace_integrations(['requests']) 
 
 app = Flask(__name__)
 app.config.from_pyfile('config_file.cfg')
@@ -27,14 +40,19 @@ r = redis.Redis()
 if not r.get(button1): r.set(button1, 0)
 if not r.get(button2): r.set(button2, 0)
 
+
 logger = logging.getLogger(__name__)
-logger.addHandler(AzureLogHandler(connection_string=f'InstrumentationKey={APP_INSIGHTS_KEY}'))
+handler = AzureLogHandler(connection_string=f'InstrumentationKey={APP_INSIGHTS_KEY}')
+handler.setFormatter(logging.Formatter('%(traceId)s %(spanId)s %(message)s'))
+logger.addHandler(handler)
+logger.addHandler(AzureEventHandler(connection_string=f'InstrumentationKey={APP_INSIGHTS_KEY}'))
 logger.setLevel(logging.INFO)
 
 exporter = metrics_exporter.new_metrics_exporter(
     enable_standard_metrics=True,
     connection_string=f'InstrumentationKey={APP_INSIGHTS_KEY}'
 )
+view_manager.register_exporter(exporter)
 
 tracer = Tracer(
     exporter=AzureExporter(connection_string=f'InstrumentationKey={APP_INSIGHTS_KEY}'),
@@ -51,11 +69,8 @@ middleware = FlaskMiddleware(
 def index():
     if request.method == 'GET':
         vote1 = r.get(button1).decode('utf-8')
-        with tracer.span(name="Cats Vote GET"):
-            pass
+        
         vote2 = r.get(button2).decode('utf-8')
-        with tracer.span(name="Dogs Vote GET"):
-            pass
 
         return render_template(
             "index.html",
@@ -75,8 +90,7 @@ def index():
             vote1 = r.get(button1).decode('utf-8')
             vote2 = r.get(button2).decode('utf-8')
 
-            logger.info('Cats vote reset', extra={"custom_dimensions": {"Cats Vote": vote1}})
-            logger.info('Dogs vote reset', extra={"custom_dimensions": {"Dogs Vote": vote2}})
+            logger.info('Reset event triggered', extra={'custom_dimensions': {'Cats Vote Count': vote1, 'Dogs Vote Count': vote2}})
 
             return render_template(
                 "index.html",
@@ -90,9 +104,16 @@ def index():
             r.incr(vote, 1)
 
             with tracer.span(name=f"Vote Recorded: {vote}"):
-                pass
+                print(f"Vote Recorded: {vote}")
 
-            logger.info(f'{vote} vote clicked', extra={"custom_dimensions": {"Voted For": vote}})
+            logger.info(
+                f'{vote} Vote Clicked', 
+                extra={
+                    "custom_dimensions": {
+                        "Voted For": vote
+                    }
+                }
+            )
 
             vote1 = r.get(button1).decode('utf-8')
             vote2 = r.get(button2).decode('utf-8')
